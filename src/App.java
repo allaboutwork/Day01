16154f04d0cf34fe9f398922ec5152a845cb2c5e
allaboutwork <afshanalamwork@outlook.com>
2023-03-21 16:42:07 +0530

    day 1 work

---
+++ src/App.java
@@ -0,0 +1,96 @@
+// DAY 1 OF CODING INTERNSHIP
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        // System.out.println("Hello, World!");
+        // int a = 4;
+        // int b = 5;
+        // double d = 5.3;
+        // boolean t = true;
+        // boolean t1 = false;
+        // String s = "java";
+        // char c = 'j';
+
+        // int age = 20;
+        // if(age>18){
+        //     System.out.println("can vote");
+        // }else{
+        //     System.out.println("can not vote");
+        // }
+
+        // for (int i = 0; i < 5; i++) {
+        //     System.out.println("Hello");
+        // } 
+
+        // while (condition:var(boolean)) {
+            
+        // }
+
+        // do {
+            
+        // } while (condition);
+
+        // find factorial
+        // int fact = 1;
+        // for (int i = 1; i <= 5; i++) {
+        //     fact = fact * i;
+        // }
+        // System.out.println("factorial of 5  : " + fact);
+    
+        // function
+        // fact(4);
+
+        // create a fucntion and count the number of digits
+        // countDigit(8521);
+
+        // create a function and reverse a number
+        // System.out.println(reverseNumber(542));
+        
+        // create a function and check palindrome
+        // isPlaindrome(545);
+    }
+
+    // check palimdrome of a number
+    // public static void isPlaindrome(int n) {
+
+    //     // process
+    //     if(n == reverseNumber(n)){
+    //         System.out.println("This is a palindrome.");              
+    //     }
+    // }
+
+    // reverse a number
+    // public static int reverseNumber(int n) {
+    //     // variable
+    //     int rev = 0;
+    //     int temp = 0;
+
+    //     // process
+    //     while(n > 0){
+    //         temp = n%10;
+    //         rev = rev*10 + temp;
+    //         n = n/10;                
+    //     }
+        
+    //     return rev;
+    // }
+
+    // find no. of digits
+    // public static void countDigit(int n) {
+    //     int count = 0;
+    //     while(n!=0){
+    //         n = n/10;
+    //         count++;
+    //     }
+    //     System.out.println("No. of digits: " + count);
+    // }
+
+    // find factorial
+    // public static void fact(int n) {
+    //     int fact = 1;
+    //     for (int i = 1; i <= n; i++) {
+    //         fact = fact * i;
+    //     }
+    //     System.out.println("factorial of "+ n +"  : " + fact);
+    // }
+}
